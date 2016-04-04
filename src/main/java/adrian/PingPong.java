7bd27292c97d852d2bad32b57cbd7effb8d33a0b
Adrian O'Sullivan <adrian.osullivan@gmail.com>
2016-04-04 13:23:38 -0400

    Formatted

--- src/main/java/adrian/PingPong.java
+++ src/main/java/adrian/PingPong.java
@@ -10,86 +10,94 @@ import akka.japi.pf.FI.UnitApply;
 import akka.japi.pf.ReceiveBuilder;
 
 public class PingPong {
-	
-	static class PingMessage{};
-	static class PongMessage{};
-	static class StartMessage{};
-	static class StopMessage{};	
-	
-	static class Ping extends AbstractActor {
-		
-		private ActorRef pong;
-		int count = 0;
-		
-		public Ping(ActorRef pong) {
-			System.err.println("ping constructor");
-			this.pong = pong;
-		}
 
-		@Override
-		public PartialFunction<Object, BoxedUnit> receive() {
-			
-			System.err.println("Ping receive");
-			
-			UnitApply<PongMessage> pongHandler = message -> { System.err.println("ping " + count + ' ' + Thread.currentThread().getName());
-			  										 count += 1;
-			  										 if (count > 1025) {
-			  											sender().tell(new StopMessage(), self());
-			  											System.err.println("ping stopped");
-			  								            context().stop(self());
-			  										 } else {
-			  											 sender().tell(new PingMessage(), self());             									   
-			  										 }};
-			  										 
-			UnitApply<StartMessage> startHandler = message -> { System.err.println("ping start");
-			  										  count += 1;
-			  										  pong.tell(new PingMessage(), self()); };
-			  									      	
-			return ReceiveBuilder
-		              .match(StartMessage.class, startHandler)
-		              .match(PongMessage.class, pongHandler)		              										 
-		              .build();
-			
-		}
-		
-	}
-	
-	static class Pong extends AbstractActor {
-		
-		@Override
-		public PartialFunction<Object, BoxedUnit> receive() {
-			
-			System.err.println("Pong receive");
-			
-			UnitApply<PingMessage> pingHandler = message -> { System.err.println("pong " + Thread.currentThread().getName());
-              									     sender().tell(new PongMessage(), self()); };
-			UnitApply<StopMessage> stopHandler = message -> { System.err.println("pong stopped");
-              									     context().stop(self()); };
-              									     
-			return ReceiveBuilder
-              .match(PingMessage.class, pingHandler )
-              .match(StopMessage.class, stopHandler )              									   
-              .build();
-		}
-	}
-	
-	public static void main(String[] args){
-		PingPong game = new PingPong();
-		game.begin();
-	}
+    static class PingMessage {
+    };
 
-	private void begin() {
-	  ActorSystem system = ActorSystem.create("PingPongSystem");
-	  
-	  ActorRef pong = system.actorOf(Props.create(Pong.class));
-	  ActorRef ping = system.actorOf(Props.create(Ping.class, pong));
-	  
-	  // start them going
-	  ping.tell(new StartMessage(), ActorRef.noSender());
-	}
+    static class PongMessage {
+    };
 
-	
+    static class StartMessage {
+    };
 
-	
-        
+    static class StopMessage {
+    };
+
+    static class Ping extends AbstractActor {
+
+        private ActorRef pong;
+        int count = 0;
+
+        public Ping(ActorRef pong) {
+            System.err.println("ping constructor");
+            this.pong = pong;
+        }
+
+        @Override
+        public PartialFunction<Object, BoxedUnit> receive() {
+
+            System.err.println("Ping receive");
+
+            UnitApply<PongMessage> pongHandler = message -> {
+                System.err.println("ping " + count + ' '
+                        + Thread.currentThread().getName());
+                count += 1;
+                if (count > 1025) {
+                    sender().tell(new StopMessage(), self());
+                    System.err.println("ping stopped");
+                    context().stop(self());
+                } else {
+                    sender().tell(new PingMessage(), self());
+                }
+            };
+
+            UnitApply<StartMessage> startHandler = message -> {
+                System.err.println("ping start");
+                count += 1;
+                pong.tell(new PingMessage(), self());
+            };
+
+            return ReceiveBuilder.match(StartMessage.class, startHandler)
+                    .match(PongMessage.class, pongHandler).build();
+
+        }
+
+    }
+
+    static class Pong extends AbstractActor {
+
+        @Override
+        public PartialFunction<Object, BoxedUnit> receive() {
+
+            System.err.println("Pong receive");
+
+            UnitApply<PingMessage> pingHandler = message -> {
+                System.err.println("pong " + Thread.currentThread().getName());
+                sender().tell(new PongMessage(), self());
+            };
+            UnitApply<StopMessage> stopHandler = message -> {
+                System.err.println("pong stopped");
+                context().stop(self());
+            };
+
+            return ReceiveBuilder.match(PingMessage.class, pingHandler)
+                    .match(StopMessage.class, stopHandler).build();
+        }
+    }
+
+    public static void main(String[] args) {
+        PingPong game = new PingPong();
+        game.begin();
+    }
+
+    private void begin() {
+        ActorSystem system = ActorSystem.create("PingPongSystem");
+
+        ActorRef pong = system.actorOf(Props.create(Pong.class));
+        ActorRef ping = system.actorOf(Props.create(Ping.class, pong));
+
+        // start them going
+        ping.tell(new StartMessage(), ActorRef.noSender());
+    }
+
 }
